a3851e956586d23b2ed8b6fac71ed9d0b3e34c0f
Henrique Estima <henriqueestma@gmail.com>
2026-09-05 17:48:42 -0300

    feat: adiciona backend inicial e validacoes de servicos

---
+++ backend/src/app.js
@@ -0,0 +1,99 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const servicos = [
+    {
+        id: 1,
+        servico: "alongamento",
+        valor: 150,
+        imagem: "foto1"
+    },
+    {
+        id: 2,
+        servico: "manutenção",
+        valor: 80,
+        imagem: "foto1"
+    }
+]
+
+const imagens = [
+    {
+        foto: 1
+    },
+    {
+        foto: 2
+    }
+]
+
+function buscaServ(id) {
+    return servicos.findIndex(servicos => {
+        return servicos.id === Number(id);
+    })
+}
+
+app.get("/", (req, res) => {
+    res.status(200).send("NailsHub app.js")
+});
+
+app.get("/servicos", (req, res) => {
+    res.status(200).json(servicos);
+});
+
+app.get("/servicos/:id", (req, res) => {
+    const index = buscaServ(req.params.id);
+    res.status(200).json(servicos[index]);
+})
+
+app.post("/servicos", (req, res) => {
+
+    if(req.body.servico === ""){
+        return res.status(400).send("Nome do serviço obrigatório, ação cancelada")
+    }
+
+    if(req.body.valor <= 0){
+        return res.status(400).send("Valor inválido, necessita ser maior à 0");
+    }
+    
+    const servicoExiste = servicos.some(servico => servico.servico.toLocaleLowerCase() === req.body.servico.toLowerCase());
+    
+    if (servicoExiste === true){
+       return res.status(409).send("Serviço duplicado, ação cancelada");
+    };
+
+    let novoID = 1;
+
+    while (servicos.some(servico => servico.id === novoID)){
+        novoID++;
+    };
+
+    const novoServiço = {
+        id: novoID,
+        servico: req.body.servico,
+        valor: req.body.valor,
+        ativo: true,
+        imagens: "foto1"
+    };
+
+    servicos.push(novoServiço);
+    
+    res.status(201).json(novoServiço);
+});
+
+app.put("/servicos/:id", (req, res) => {
+    const index = buscaServ(req.params.id);
+    servicos[index].servico = req.body.servico;
+    res.status(200).json(servicos);
+})
+
+app.get("/imagens", (req, res) => {
+    res.status(200).json(imagens);
+});
+
+app.post("/imagens", (req, res) => {
+    imagens.push(req.body);
+    res.status(201).send("Imagem salva.");
+});
+
+export default app;
